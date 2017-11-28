63440096809dda495d843cf97eed699e0d4bdad3
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-28 01:31:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  horiz_divider = "-----------\n"
-  line = "   |   |   \n"
+  horiz_divider = "\n-----------\n"
+  cell = "   "
+  vert = "|"
+  line = cell + vert + cell + vert + cell
   print line
   print hiroz_divider
   print line
   print hiroz_divider
   print line
+  print "\n""
 end
